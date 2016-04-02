14efbfd02e3fecd4492f73e5a2fa513ce5f5da19
Николай Прудковский < >
2016-04-02 14:37:13 +0300

    qweqwer

---
+++ lab_4.cpp
@@ -0,0 +1,349 @@
+// lab_4.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class numeric {
+	int number, limit;
+public:
+	numeric() {
+		number = 0;
+		limit = 0;
+	}
+	~numeric() {}
+	numeric(int k, int l) {
+		number = k;
+		limit = l;
+		//cout << "Creating numeric" << endl;
+	}
+	numeric& operator+(numeric& op2)
+	{
+		numeric temp;
+		temp.number = number + op2.number;
+		return temp;
+	}
+
+	numeric& operator*(numeric& op2)
+	{
+		numeric temp;
+		temp.number = number * op2.number;
+		return temp;
+	}
+};
+
+class matrix {
+	numeric ** p;
+	int n,m;
+public:
+	matrix() {
+		n = 1;
+		m = 1;
+		p = new numeric*[n];
+		p[0] = new numeric[m];
+	}
+
+	matrix(int rows, int cols) {
+		n = rows;
+		m = cols;
+		p = new 
+	}
+
+	~matrix()
+	{
+		//cout << "vector dtor" << endl;
+	}
+
+	numeric& operator[](int index)
+	{
+		return p[index];
+	}
+
+	matrix& operator *(matrix& sch)
+	{
+		matrix temp; numeric comp;
+		for (int i = 0; i < n - 1; i++)
+		{
+			for (int j = 0; j < sch.n - 1; j++) {
+				comp = p[i] * sch.p[j];
+			    temp.p[i] = temp.p[i] + comp;
+			}
+		}
+		return temp;
+	}
+
+	void show();
+};
+
+void matrix::show() {
+	for (int i = 0; i < n - 1; i++)
+		for (int j = 0; j < m - 1; j++)
+			cout << p[i, j].count << " ";
+	cout << "\n" << endl;
+}
+
+
+int _tmain()
+{
+	int num, lim = 0;
+	int N; //строки, столбцы
+	matrix m1, m2, m3;
+
+	cout << "n*n : N = ";
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+	{
+		cout << i << " string" << endl;
+		for (int j = 0; j < N; j++)
+		{
+			cin >> m1.
+		}
+	}
+			cout << "Number mat1 = "; cin >> num;
+			numeric a(num, lim);
+			v1 = v1 + a;
+
+			cout << "Number mat2 = "; cin >> num;
+			numeric b(num, lim);
+			v2 = v2 + b;
+		
+		cout << "if you want to continue, enter 'ok' " << endl;
+		cin >> c;
+	}
+
+	//v3 = v1 * v2;
+	v1.show();
+	v2.show();
+	v3.show();
+
+	//v[0].show();
+	//v2[0].show();
+
+	system("pause");
+	return 0;
+}
+
+/*
+class director : {
+protected:
+	string direct;
+public:
+	director() : school() {
+		direct = "default";
+	}
+	virtual ~director() {
+		//cout << "director dtor" << endl;
+	}
+	director(string s, int count, string dir) : school(s, count) {
+		direct = dir;
+		//cout << "Creating Public School" << endl;
+	}
+	void show();
+};
+
+class specialschool : public director {
+protected:
+	string speciality;
+public:
+	specialschool() : director() {
+		speciality = "default";
+	}
+	~specialschool() {
+		//cout << "specialschool dtor" << endl;
+	}
+	specialschool(string s, int count, string dir) : director(s, count, dir) {
+		//cout << "Creating Special School" << endl;
+	}
+	void show();
+
+	specialschool& operator=(string s) {
+		speciality = s;
+		return *this;
+	}
+
+	string getname() {
+		return name;
+	}
+};
+
+class Vector {
+	specialschool *p;
+	int n;
+public:
+	Vector() {
+		n = 1;
+		p = new specialschool[n];
+	}
+
+	~Vector()
+	{
+		//cout << "vector dtor" << endl;
+	}
+
+	int getcount() {
+		return n;
+	}
+
+	specialschool& operator[](int index)
+	{
+		return p[index];
+	}
+
+	Vector& operator +(specialschool& sch)
+	{
+		n++;
+		specialschool *save;
+		save = p;
+		p = new specialschool[n];
+		for (int i = 0; i < n - 1; i++)
+			p[i] = save[i];
+		p[n - 2] = sch;
+		return *this;
+	}
+
+	Vector& operator -(specialschool& sch)
+	{
+		int k = -1;
+		for (int i = 0; i < n - 1; i++)
+			if (p[i].getname() == sch.getname()) {
+				k = i;
+			}
+
+		if (k != -1) {
+			n--;
+			specialschool *save;
+			save = p;
+			p = new specialschool[n];
+			for (int i = 0; i < n - 1; i++)
+				if (i < k) p[i] = save[i];
+				else p[i] = save[i + 1];
+		}
+		return *this;
+	}
+
+	Vector& operator &(Vector& sch)
+	{
+		Vector temp;
+		for (int i = 0; i < n - 1; i++)
+			for (int j = 0; j < sch.n - 1; j++)
+				if (p[i].getname() == sch.p[j].getname())
+					temp + p[i];
+		return temp;
+	}
+
+	Vector& operator |(Vector& sch)
+	{
+		Vector temp, peresech;
+		for (int i = 0; i < n - 1; i++)
+			temp + p[i];
+		for (int j = 0; j < sch.n - 1; j++)
+			temp + sch.p[j];
+
+		peresech = *this & sch;
+		for (int k = 0; k < peresech.n - 1; k++)
+			temp - peresech.p[k];
+		return temp;
+	}
+
+	void show();
+};
+
+class functor {
+	Vector temp;
+public:
+	void operator()(Vector a, Vector b) {
+		temp = a & b;
+		cout << "\nI'm functor!!!" << endl;
+		cout << "number of schools Vector1 & Vector2 = " << temp.getcount() - 1 << endl;
+	}
+};
+
+void director::show() {
+	cout << "\nSchool " << name << endl;
+	cout << "Director " << direct << endl;
+	cout << "number of students = " << number << endl;
+}
+
+void specialschool::show() {
+	cout << "\nSchool " << name << endl;
+	cout << "Director " << direct << endl;
+	cout << "number of students = " << number << endl;
+	cout << "Speciality: " << speciality << endl;
+}
+
+void Vector::show() {
+	for (int i = 0; i < n - 1; i++)
+		p[i].show();
+}
+
+int _tmain()
+{
+	string name, speciality, direct, c = "ok";
+	int num;
+	Vector v, v1, v2, v3;
+
+	specialschool* b = new specialschool;
+	director* a = b;
+	delete a;
+
+
+	while (c == "ok")
+	{
+		cout << "add object -> '+' or clean object -> '-' " << endl;
+		cin >> c;
+
+		if (c == "+") {
+			cout << "\nSchool             "; cin >> name;
+			cout << "Director             "; cin >> direct;
+			cout << "Count of students    "; cin >> num;
+			cout << "Speciality           "; cin >> speciality;
+			specialschool g(name, num, direct);
+			g = speciality;
+			v = v + g;
+
+			cout << "\nSchool             "; cin >> name;
+			cout << "Director             "; cin >> direct;
+			cout << "Count of students    "; cin >> num;
+			cout << "Speciality           "; cin >> speciality;
+			specialschool l(name, num, direct);
+			g = speciality;
+			v1 = v1 + l;
+		}
+		if (c == "-") {
+			cout << "\nSchool               "; cin >> name;
+			specialschool g(name, num, direct);
+			g = speciality;
+			v = v - g;
+
+			cout << "\nSchool               "; cin >> name;
+			specialschool l(name, num, direct);
+			g = speciality;
+			v1 = v1 - l;
+		}
+		cout << "if you want to continue, enter 'ok' " << endl;
+		cin >> c;
+	}
+
+	v2 = v & v1;
+	v3 = v | v1;
+	functor f;
+	f(v, v1);
+
+	//v[0].show();
+	//v2[0].show();
+
+	cout << "\nVECTOR1" << endl;
+	v.show();
+	cout << "\nVECTOR2" << endl;
+	v1.show();
+	cout << "\nVECTOR1 & VECTOR2" << endl;
+	v2.show();
+	cout << "\nVECTOR1 | VECTOR2" << endl;
+	v3.show();
+	system("pause");
+	return 0;
+} */
