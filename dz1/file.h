826fe7766c82893d227235b9958ab9efcd22d33e
Prudkovskiy <nickolay.prud@gmail.com>
2016-06-09 14:13:13 +0300

    add history

--- dz1/file.h
+++ dz1/file.h
@@ -7,12 +7,14 @@ using namespace std;
 class file : public parent {
 private:
 	string data;
+	vector <string> history;
 public:
 	file(string, string, vector<user*>, directory*, string);
 	~file();
 	void rename(user, string);
 	void redact(user, string);
 	string getdata(user);
+	vector<string> get_history();
 };
 
 file::file(string fname = "default", string adr = "default", vector<user*> users_, directory* dir = NULL, string fdata = "default") :
@@ -21,37 +23,56 @@ file::file(string fname = "default", string adr = "default", vector<user*> users
 	data = fdata; 	
 }
 
-file::~file() { cout << "dctor file"; }
+file::~file() { 
+	history.clear();
+	cout << "dctor file";
+}
 
 void file::rename(user us, string fname)
 {
 	int k = 0;
 	while ((k < users.size()) && (us.name != users[k]->name))
 		k++;
-	if ((k != users.size()) || (us.get_adm == true))
+	if ((k != users.size()) || (us.get_adm == true)) {
+		history.push_back("success rename_file by " + us.name);
 		name = fname;
-	else
+	}
+	else {
+		history.push_back("fail rename_file by " + us.name);
 		throw 404;
+	}
 }
 
-void file::redact(user user, string fdata)
+void file::redact(user us, string fdata)
 {
 	int k = 0;
-	while ((k < users.size()) && (user.name != users[k]->name))
+	while ((k < users.size()) && (us.name != users[k]->name))
 		k++;
-	if ((k != users.size()) || (user.get_adm == true))
+	if ((k != users.size()) || (us.get_adm == true)) {
+		history.push_back("success write_in_file by " + us.name);
 		data = fdata;
-	else
+	}
+	else {
+		history.push_back("fail write_in_file by " + us.name);
 		throw 404;
+	}
 }
 
-string file::getdata(user user) 
+string file::getdata(user us) 
 { 
 	int k = 0;
-	while ((k < users.size()) && (user.name != users[k]->name))
+	while ((k < users.size()) && (us.name != users[k]->name))
 		k++;
-	if ((k != users.size()) || (user.get_adm == true))
+	if ((k != users.size()) || (us.get_adm == true)) {
+		history.push_back("success read_file by " + us.name);
 		return data;
-	else
+	}
+	else {
+		history.push_back("fail read_file by " + us.name);
 		throw 404;
+	}		
+}
+
+vector<string> file::get_history() {
+	return history;
 }
